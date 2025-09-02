ee1c07c0249272245a6702e0781b882f3d9e5540
dianannperla <waafirmelyza@hotmail.com>
2025-09-02 17:24:42 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Chào mừng đến với Trang Web Giới Thiệu Sản Phẩm</h1>
+        <p>
+          Đây là nơi bạn có thể tạo ra các trang web giới thiệu sản phẩm một cách dễ dàng.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
